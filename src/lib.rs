2ecd6a0edfc109093db7f3afa9c320261db7d670
matthiasseitz <matthias.seitz@tum.de>
2019-09-27 19:28:14 +0200

    clean up mod layout

--- src/lib.rs
+++ src/lib.rs
@@ -4,24 +4,8 @@ extern crate derive_builder;
 
 extern crate nalgebra as na;
 
-/// Every equation has a Problem type, a solution type, and the same solution handling setup.
-pub mod gaussian;
-pub mod hessian;
-pub mod jacobian;
-
-pub mod error;
-
-pub mod ode;
-
 pub mod bvp;
-
+/// Every equation should hav a Problem type, a solution type, and the same solution handling setup.
+pub mod error;
+pub mod ode;
 pub mod sde;
-
-pub mod alg;
-
-#[cfg(feature = "problems")]
-pub mod problems;
-
-pub mod types;
-
-pub mod algorithms;
--- src/problems/mod.rs
+++
@@ -1,2 +0,0 @@
-// https://github.com/JuliaDiffEq/DiffEqProblemLibrary.jl
-pub mod ode;
